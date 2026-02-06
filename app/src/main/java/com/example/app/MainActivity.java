270350dbc7b76733336e874820b49a80b60d2207
EgnitionX <skiyemba@live.com>
2026-02-06 13:49:18 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.app;
+package com.slymax.webview;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
@@ -12,30 +13,22 @@ public class MainActivity extends Activity {
 
     @Override
     @SuppressLint("SetJavaScriptEnabled")
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    
-    mWebView = findViewById(R.id.activity_main_webview);
-    WebSettings webSettings = mWebView.getSettings(); // Fixed the dash to an equals sign
-    
-    // The "Magic Three" lines for React/Vite apps:
-    webSettings.setJavaScriptEnabled(true);
-    webSettings.setDomStorageEnabled(true);           // Fixed the 'truel' typo
-    webSettings.setDatabaseEnabled(true);            // Added for extra stability
-    
-    mWebView.setWebViewClient(new MyWebViewClient());
-    
-    // Make sure this line exists to actually start the load!
-    mWebView.loadUrl(getString(R.string.website_url));
-}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        mWebView = findViewById(R.id.activity_main_webview);
+        
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
 
-        // REMOTE RESOURCE
-        // mWebView.loadUrl("https://cool-chaja-b1ab61.netlify.app/");
-
-        // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+        mWebView.setWebViewClient(new WebViewClient());
+        
+        // This pulls the URL you set in your strings.xml
+        mWebView.loadUrl(getString(R.string.website_url));
     }
 
     @Override
@@ -47,3 +40,4 @@ protected void onCreate(Bundle savedInstanceState) {
         }
     }
 }
+
